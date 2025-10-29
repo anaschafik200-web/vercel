5501ba13c871e3d551a4292bad2047b6312c0d8d
anaschafik200-web <anaschafik200@gmail.com>
2025-10-29 15:48:17 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,4 @@
-import type { Metadata } from "next";
 import "./globals.css";
-
-export const metadata: Metadata = {
-	title: "Tweet Clone - Video Post",
-	description: "A Twitter-style video post clone",
-};
 
 export default function RootLayout({
 	children,
